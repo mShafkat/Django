3e4c56560b35c9a8be860d12f2d2c706f58033d6
mShafkat <shafkatlabib2001@gmail.com>
2025-09-11 18:37:12 +0600

    .

--- first/first/settings.py
+++ first/first/settings.py
@@ -16,6 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+static_DIR = os.path.join(BASE_DIR, 'static')
 
 
 
@@ -123,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [static_DIR,]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
