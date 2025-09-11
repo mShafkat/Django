bfa9bdaccfde4aee8fc323afbcffafcd244f25fe
mShafkat <shafkatlabib2001@gmail.com>
2025-09-11 17:15:14 +0600

    added new files and fixed some code

--- first/payments/views.py
+++ first/payments/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
+from datetime import datetime
 
 
 # Create your views here.
 def bk(request):
-    payments_method = "bkash"
-    discount = "10%"
-    condition = 'Only for first time users'
-    payments_info = {'pm': payments_method, 'dis': discount, 'con': condition}
-    return render(request, "payments/payments1.html", payments_info)
+
+    d = datetime.now()
+
+    # payments_method = "bkash"
+    # discount = "10%"
+    # condition = 'Only for first time users'
+    # payments_info = {'pm': payments_method, 'dis': discount, 'con': condition}
+    # return render(request, "payments/payments1.html", payments_info)
+    return render(request, "payments/payments1.html", {'date': d})
 
 def rk(request):
-    payments_method = "rocket"
-    discount = "15%"
-    condition = 'Only for first time users'
-    payments_info = {'pm': payments_method, 'dis': discount, 'con': condition}
-    return render(request, "payments/payments2.html", payments_info)
+    d = datetime.now()
+    # payments_method = "rocket"
+    # discount = "15%"
+    # condition = 'Only for first time users'
+    # payments_info = {'pm': payments_method, 'dis': discount, 'con': condition}
+    # return render(request, "payments/payments1.html", payments_info)
+    return render(request, "payments/payments2.html", {'date': d})
